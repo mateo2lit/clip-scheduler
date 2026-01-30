755f1d647b6ddbc940e13a9da21f41a436dbf65c
David <dbh28tekkit@gmail.com>
2026-01-30 00:12:15 -0500

    Fix Next.js config for Vercel (use next.config.js)

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Keep this empty unless you KNOW you need something specific.
+  // Add options below as needed.
+};
+
+module.exports = nextConfig;
